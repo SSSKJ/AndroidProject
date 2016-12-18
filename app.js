6d7866efeca1d69f835d9ecfbbf4601e53db8574
SSSKJ <438841268@qq.com>
2016-12-18 23:58:02 +0800

    添加数据库依赖和session依赖

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var api = require('./routes/main');
+//var users = require('./routes/users');
 
 var app = express();
 
@@ -21,9 +22,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'android server',
+  resave: false,
+  saveUninitialized: false
+}));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', api);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var mongojs = require('mongojs')
 var router = express.Router();
+
+var db = mongojs(connectionString, [collections])
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/login', function(req, res, next) {
+  res.send('respond');
 });
 
 module.exports = router;
