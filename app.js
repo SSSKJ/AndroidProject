8a60b2827d60a744fc20f8176d85b7f5d0ebaa30
SKJ <438841268@qq.com>
2017-01-05 14:09:47 +0800

    完成基本的登录机制,实现了数据库读写和session,更新readme

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ app.use(session({
 }));
 
 app.use('/', api);
-//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/main.js
+++ routes/main.js
@@ -1,17 +1,48 @@
 var express = require('express');
-var mongojs = require('mongojs')
+var mongojs = require('mongojs');
 var router = express.Router();
 
-var db = mongojs(connectionString, [collections])
+var connectionString = 'localhost:27017/ListDB';
+var collection = ['users'];
+
+var db = mongojs(connectionString, collection);
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
+});*/
 
 router.post('/login', function(req, res, next) {
-  res.send('respond');
+  db.users.findOne({username:req.body.username}, function(err, user) {
+    if (err) res.send('Server Error');
+    if (!user) res.send('用户名不存在');
+    else {
+      if (user.password == req.body.password) {
+      req.session.username = req.body.username;
+      res.send('OK');
+      } else {
+        res.send('密码错误');
+      }
+    }
+  });
+});
+
+router.post('/register', function(req, res, next) {
+
+  db.users.findOne({username:req.body.username}, function(err, user) {
+    if (err) res.send('Server Error');
+
+    if (!user) {
+      db.users.save({username:req.body.username, password:req.body.password}, function(err, saved) {
+        if (err || !saved) res.send("Server Error");
+        else  {
+          req.session.username = req.body.username;
+          res.send("OK");
+        }
+      });
+    } else res.send("用户名已存在");
+  });
 });
 
 module.exports = router;
